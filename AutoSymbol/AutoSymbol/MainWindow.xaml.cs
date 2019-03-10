412bad9ce4e82d9de5a509208846dbc0fead8547
zifengHe <zifengh@gmail.com>
2019-03-10 13:07:50 -0700

    First Symbol class

---
+++ AutoSymbol/AutoSymbol/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.WpfGraphControl;
+
+namespace AutoSymbol
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {            
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            GraphViewer graphViewer = new GraphViewer();
+            graphViewer.BindToPanel(ContentPanel);
+            Graph graph = new Graph();
+
+            graph.AddEdge("A", "B");
+            graph.Attr.LayerDirection = LayerDirection.LR;
+            graphViewer.Graph = graph; // throws exception
+        }
+
+        private void RunClicked(object sender, RoutedEventArgs e)
+        {
+           // new Builder().BuildPattern0();
+        }
+    }
+}
